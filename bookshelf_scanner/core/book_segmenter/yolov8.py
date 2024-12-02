a4fdf31a295b149424bd39902d120d029107b31c
sullivanmsean <sullivan.sean2@northeastern.edu>
2024-12-02 15:24:44 -0500

    edits to PATHs to make dynamic

--- bookshelf_scanner/core/book_segmenter/yolov8.py
+++ bookshelf_scanner/core/book_segmenter/yolov8.py
@@ -9,12 +9,16 @@ class YOLO_model:
     YOLO model class for book detection
     """
     def __init__(self, model_path):
+        import os
+        if model_path is None:
+            model_path = os.path.join(os.path.dirname(__file__), "models/OpenShelves8.onnx") #dynamic compute of path  to ONNX model
+            model_path = os.path.abspath(model_path)
         self.init_model(model_path)
         self.confidence_threshold = 0.3
         self.iou_threshold = 0.5
 
 
-    def init_model(self, model_path):
+    def init_model(self, model_path=None):
         self.ort_session = ort.InferenceSession(model_path)
         self.input_name = self.ort_session.get_inputs()[0].name
         self.output0_name = self.ort_session.get_outputs()[0].name
